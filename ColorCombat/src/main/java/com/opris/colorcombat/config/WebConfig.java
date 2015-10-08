9053190ddc8038e435f51fdc4bd8a939ab8c333b
boris <boris_traktor@mail.ru>
2015-10-08 23:54:14 +0500

    Поправил подключение js.

--- ColorCombat/src/main/java/com/opris/colorcombat/config/WebConfig.java
+++ ColorCombat/src/main/java/com/opris/colorcombat/config/WebConfig.java
@@ -26,8 +26,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/views/**").addResourceLocations("/views/");
-//        registry.addResourceHandler("/JS/**").addResourceLocations("/JS/");
-    }
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 
     @Bean
     public ViewResolver viewResolver() {
--- ColorCombat/src/main/webapp/views/JSP/game.jsp
+++ ColorCombat/src/main/webapp/views/JSP/game.jsp
@@ -12,7 +12,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
-        <script src="<c:url value="/views/socketJS.js" />"></script>
+        <script src="<c:url value="/js/socketJS.js" />"></script>
     </head>
     <body>
         <h1>Hello World!</h1>
